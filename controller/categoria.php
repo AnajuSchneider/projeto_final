a2bb8facc95fb89e0eef4fd56c9bb505e82c42f1
Ana JÃºlia Schneider <15015143699@dc01.ifs.ifsuldeminas.edu.br>
2022-06-29 16:55:59 -0300

    front-controller

--- controller/categoria.php
+++ controller/categoria.php
@@ -1,10 +1,25 @@
 <?php
 
-require "../model/categoriaModel.php";
+require "model/categoriaModel.php";
 
-$categoria = new categoriaModel();
-$categoria->inserir("Produto Limpeza");
-$categoria->excluir(1);
-$categoria->atualizar(2, "SmartPhone");
-var_dump($categoria->buscarPorId(3));
-var_dump($categoria->buscarTudo());
+    class categoria{
+        function __construct() {
+            $this->modelo = new categoriaModel();
+        }
+        function index(){
+            include "view/template/conteudo.php";
+        }
+        function add(){
+            echo "mostrar form categoria";
+        }
+        function excluir($id){
+            echo "excluir categoria";
+        }
+    }
+
+//$categoria = new categoriaModel();
+//$categoria->inserir("Produto Limpeza");
+//$categoria->excluir(1);
+//$categoria->atualizar(2, "SmartPhone");
+//var_dump($categoria->buscarPorId(3));
+//var_dump($categoria->buscarTudo());
--- model/categoriaModel.php
+++ model/categoriaModel.php
@@ -1,6 +1,6 @@
 <?php
 
-require "../config/conexao.php";
+require "config/conexao.php";
 
 class categoriaModel{
     var $conexao;
